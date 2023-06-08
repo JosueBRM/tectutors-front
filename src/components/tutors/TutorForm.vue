76a93d2f9b8c8628b8c7afb7b63802d8cd30423e
josuebrm <josuederker2022@gmail.com>
2023-06-08 17:12:30 -0600

    Fixed register error

--- src/components/tutors/TutorForm.vue
+++ src/components/tutors/TutorForm.vue
@@ -1,158 +1,165 @@
 <template>
-    <form @submit.prevent="submitForm">
+  <form @submit.prevent="submitForm">
 
-        <div class="form-control" :class="{invalid: !firstName.isValid}">
-            <label for="firstname">Nombre</label>
-            <input type="text" id="fistname" v-model.trim="firstName.val"/>
-            <p v-if="!firstName.isValid">First name must not be empty.</p>
-        </div>
+    <div class="form-control" :class="{ invalid: !firstName.isValid }">
+      <label for="firstname">Nombre</label>
+      <input type="text" id="fistname" v-model.trim="firstName.val" @blur="clearValidity('firstName')" />
+      <p v-if="!firstName.isValid">First name must not be empty.</p>
+    </div>
 
-        <div class="form-control" :class="{invalid: !lastName.isValid}">
-            <label for="lastname">Apellido</label>
-            <input type="text" id="lastname" v-model.trim="lastName.val"/>
-            <p v-if="!lastName.isValid">Last name must not be empty.</p>
-        </div>
+    <div class="form-control" :class="{ invalid: !lastName.isValid }">
+      <label for="lastname">Apellido</label>
+      <input type="text" id="lastname" v-model.trim="lastName.val" @blur="clearValidity('lastName')" />
+      <p v-if="!lastName.isValid">Last name must not be empty.</p>
+    </div>
 
-        <div class="form-control" :class="{invalid: !email.isValid}">
-            <label for="email">E-mail</label>
-            <input type="text" id="email" v-model.trim="email.val"/>
-            <p v-if="!email.isValid">Email must not be empty.</p>
-        </div>
+    <div class="form-control" :class="{ invalid: !email.isValid }">
+      <label for="email">E-mail</label>
+      <input type="text" id="email" v-model.trim="email.val" @blur="clearValidity('email')" />
+      <p v-if="!email.isValid">Email must not be empty.</p>
+    </div>
 
-        <div class="form-control" :class="{invalid: !description.isValid}">
-            <label for="description">Descripcion</label>
-            <textarea id="description" rows="5" v-model.trim="description.val"></textarea>
-            <p v-if="!description.isValid">Description must not be empty.</p>
-        </div>
+    <div class="form-control" :class="{ invalid: !description.isValid }">
+      <label for="description">Descripcion</label>
+      <textarea id="description" rows="5" v-model.trim="description.val" @blur="clearValidity('description')"></textarea>
+      <p v-if="!description.isValid">Description must not be empty.</p>
+    </div>
 
-        <div class="form-control" :class="{invalid: !rate.isValid}">
-            <label for="rate">Tarifa por Hora</label>
-            <input type="number" id="rate" v-model.number="rate.val"/>
-            <p v-if="!rate.isValid">Rate must be greater than 0.</p>
-        </div> 
+    <div class="form-control" :class="{ invalid: !rate.isValid }">
+      <label for="rate">Tarifa por Hora</label>
+      <input type="number" id="rate" v-model.number="rate.val" @blur="clearValidity('rate')" />
+      <p v-if="!rate.isValid">Rate must be greater than 0.</p>
+    </div>
 
-        <div class="form-control" :class="{invalid: !areas.isValid}">
-            <h3>Areas de experiencia</h3>
+    <div class="form-control" :class="{ invalid: !areas.isValid }">
+      <h3>Areas de experiencia</h3>
 
-            <div>
-                <input type="checkbox" id="frontend" value="frontend" v-model="areas.val">
-                <label for="frontend">Frontend Development</label>
-            </div>
+      <div>
+        <input type="checkbox" id="frontend" value="frontend" v-model="areas.val" @blur="clearValidity('areas')">
+        <label for="frontend">Frontend Development</label>
+      </div>
 
-            <div>
-                <input type="checkbox" id="backend" value="backend" v-model="areas.val">
-                <label for="backend">Backend Development</label>
-            </div>
+      <div>
+        <input type="checkbox" id="backend" value="backend" v-model="areas.val" @blur="clearValidity('areas')">
+        <label for="backend">Backend Development</label>
+      </div>
 
-            <div>
-                <input type="checkbox" id="career" value="career" v-model="areas.val">
-                <label for="career">Career Advisory</label>
-            </div>
-            <p v-if="!areas.isValid">At least one expertise must be selected.</p>
-        </div>
+      <div>
+        <input type="checkbox" id="career" value="career" v-model="areas.val" @blur="clearValidity('areas')">
+        <label for="career">Career Advisory</label>
+      </div>
+      <p v-if="!areas.isValid">At least one expertise must be selected.</p>
+    </div>
 
-        <p v-if="!formIsValid">Plese fix the above errors and submit again.</p>
-        <base-button @click="showAlert">Register</base-button>
-    </form>
+    <p v-if="!formIsValid">Plese fix the above errors and submit again.</p>
+    <base-button @click="showAlert">Register</base-button>
+  </form>
 </template>
 
 <script>
-    export default{
-        emits:['save-data'],
-        data() {
-            return {
-                firstName: {
-                  val: '',
-                  isValid: true
-                },
-                lastName: {
-                  val: '',
-                  isValid: true
-                },
-                email: {
-                  val: '',
-                  isValid: true
-                },
-                description: {
-                  val: '',
-                  isValid: true
-                },
-                rate: {
-                  val: null,
-                  isValid: true
-                },
-                areas: {
-                  val: [],
-                  isValid: true
-                },
-                formIsValid: true
-            };
-        },
-        methods: {
-            validateForm() {
-              this.formIsValid = true;
-              if ( this.firstName.val === '' ){
-                this.firstName.isValid = false;
-                this.formIsValid = false;
-              }
-              if ( this.lastName.val === '' ){
-                this.lastName.isValid = false;
-                this.formIsValid = false;
-              }
-              if ( this.lastName.val === '' ){
-                this.lastName.isValid = false;
-                this.formIsValid = false;
-              }
-              if ( this.description.val === '' ){
-                this.description.isValid = false;
-                this.formIsValid = false;
-              }
-              if ( !this.rate.val || this.rate.val < 0 ){
-                this.rate.isValid = false;
-                this.formIsValid = false;
-              }
-              if ( this.areas.val.length === 0 ){
-                this.areas.isValid = false;
-              }
-            }, 
-            submitForm() {
-              this.validateForm();
-
-              if ( !this.formIsValid) {
-                return;
-              }
-
-                const formData = {
-                    first: this.firstName,
-                    last: this.lastName,
-                    desc: this.description,
-                    rate: this.rate,
-                    areas: this.areas
-                };
-                this.$emit('save-data', formData);
-            },
-            showAlert() {
-              if (this.formIsValid) {
-                this.$swal('Registrado Exitosamente!');
-                this.$swal.fire(
-                'Registrado!',
-                'Te registraste exitosamente!',
-                'success'
-                )
-              }else{
-                this.$swal.fire({
-                  icon: 'error',
-                  title: 'Oops...',
-                  text: 'Something went wrong!',
-                  footer: '<a href="">Why do I have this issue?</a>'
-                })
-              }
-
-              
-            },
-            
-        }
+export default {
+  emits: ['save-data'],
+  data() {
+    return {
+      firstName: {
+        val: '',
+        isValid: true
+      },
+      lastName: {
+        val: '',
+        isValid: true
+      },
+      email: {
+        val: '',
+        isValid: true
+      },
+      description: {
+        val: '',
+        isValid: true
+      },
+      rate: {
+        val: null,
+        isValid: true
+      },
+      areas: {
+        val: [],
+        isValid: true
+      },
+      formIsValid: true
     };
+  },
+  methods: {
+    clearValidity(input) {
+      this[input].isValid = true;
+    },
+    validateForm() {
+      this.formIsValid = true;
+      if (this.firstName.val === '') {
+        this.firstName.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.lastName.val === '') {
+        this.lastName.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.lastName.val === '') {
+        this.lastName.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.email.val === '') {
+        this.email.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.description.val === '') {
+        this.description.isValid = false;
+        this.formIsValid = false;
+      }
+      if (!this.rate.val || this.rate.val < 0) {
+        this.rate.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.areas.val.length === 0) {
+        this.areas.isValid = false;
+      }
+    },
+    submitForm() {
+      this.validateForm();
+
+      if (!this.formIsValid) {
+        return;
+      }
+
+      const formData = {
+        first: this.firstName.val,
+        last: this.lastName.val,
+        desc: this.description.val,
+        rate: this.rate.val,
+        areas: this.areas.val
+      };
+      this.$emit('save-data', formData);
+    },
+    showAlert() {
+      if (this.formIsValid) {
+        this.$swal('Registrado Exitosamente!');
+        this.$swal.fire(
+          'Registrado!',
+          'Te registraste exitosamente!',
+          'success'
+        )
+      } else {
+        this.$swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Something went wrong!',
+          footer: '<a href="">Why do I have this issue?</a>'
+        })
+      }
+
+
+    },
+
+  }
+};
 </script>
 
 <style scoped>
@@ -166,7 +173,7 @@ label {
   margin-bottom: 0.5rem;
 }
 
-input[type='checkbox'] + label {
+input[type='checkbox']+label {
   font-weight: normal;
   display: inline;
   margin: 0 0 0 0.5rem;
